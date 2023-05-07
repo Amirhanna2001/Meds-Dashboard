d4401c3a20dc14021d54c6db11d06174739742c7
amirhanna2001 <amirhanna111200@gmail.com>
2023-05-07 03:14:37 +0300

    Request Endpoints

--- Back-End/routes/Requests.js
+++ Back-End/routes/Requests.js
@@ -5,98 +5,91 @@ const adminAuth = require('../middleware/admin');
 const Authorized = require('../middleware/authorize');
 const util = require("util"); // helper
 
+router.get("/",Admin,async (req,res)=>{
+    const query = util.promisify(conn.query).bind(conn);
+    const requests = await query("SELECT * FROM requests");
 
-router.get('/',(req,res)=>{
-    conn.query('SELECT * FROM requests ',(error,result,fields)=>{
-        res.json(result)
-    });
+    res.status(200).json(requests);
+})
+
+router.get('/:id',async(req,res)=>{
+    const query = util.promisify(conn.query).bind(conn);
+    const user = await query("SELECT * FROM user where id = ? ",[req.params.id])
+    if(!user[0])
+        res.status(404).json({msg:"No User With ID = "+req.params.id})
+
+    const requ = await query('SELECT * FROM requests WHERE user_id = ?',[req.params.id]);
+
+    res.status(200).json(requ);
 });
 
-router.post('/:med_id',Authorized,(req,res)=>{
+router.post('/:med_id',Authorized,async(req,res)=>{
     data = req.body;
     let medicine_id = req.params.med_id
-    conn.query(
-        'INSERT INTO requests  set ? ',
-        {
-            UserId:         data.UserId,
-            medicine_id:    medicine_id,
-            request_status: 2
-
-        },(error,result,fields)=>{
-           if(error)
-               res.send("Error Please Try Again",error.stack);
-
-            res.send("Request  Added Successfully") ;
-
-        })
-
-});
-
-router.get('/GetRequest/:id',Authorized,(req,res)=>{
-    const {id} = req.params;
-    conn.query('SELECT * FROM requests WHERE ?',{id:id},(error,result,fields)=>{
-        if(error) {
-            res.statusCode = 500;
-            res.send("An Error Happend ");
-        }
-        if(result[0])
-              res.json(result[0]);
-        res.statusCode = 404;
-        res.send("Request Not Found !")
-    })
-
-});
-
-router.put('/EditRequest/:id',Admin,(req,res)=>{
-    const {id} = req.params;
-    const data = req.body; 
-    conn.query('UPDATE requests SET ? WHERE ?',
-    [{
-        UserId:         data.UserId,
-        medicine_id:    data.medicine_id,
-        request_status: data.request_status
-    },
-        {id:id}
-    ],
-        (error,result,fields)=>{
-            if(error){
-                res.statusCode = 500;
-                res.send("An Error Happend !");
-            }
-            if(result.affectedRows != 0)    
-                res.send("Request Updated ");
-
-            res.statusCode = 404;
-            res.send("Request Not Found !");
-        })
-
-});
-
-router.delete('/RefuseRequest/:id',Admin,(req,res)=>{
-    const {id} = req.params;
-    conn.query('UPDATE requests SET ? WHERE ? ',[{id:id},{request_status : 2}],
-    (error,result,fields)=>{
-        if(error){
-            res.statusCode = 500;
-            res.send("error happend");
-        }
-
-        if(result.affectedRows == 0){
-            res.statusCode = 404;
-            res.send("Request Not Found !");
-        }
-            
-        res.send("Refused");
-    })
-
-}); 
-router.get('/History',Authorized,async(req,res)=>{
+    const request = {
+        user_id: res.locals.user.ID,
+        medicine_id:req.params.med_id
+    }
     const query = util.promisify(conn.query).bind(conn);
-    console.log(res.locals.user);
-    const hist = await query('SELECT * FROM history WHERE user_id = ? ',[res.locals.user.ID])
+
+    const requ = await query('INSERT INTO  requests SET  ?',[request]);
+
+    res.status(200).json({msg:"Request Submetted "});
+
+});
+
+// router.get('//:id',Authorized,(req,res)=>{
+//     const {id} = req.params;
+//     conn.query('SELECT * FROM requests WHERE ?',{id:id},(error,result,fields)=>{
+//         if(error) {
+//             res.statusCode = 500;
+//             res.send("An Error Happend ");
+//         }
+//         if(result[0])
+//               res.json(result[0]);
+//         res.statusCode = 404;
+//         res.send("Request Not Found !")
+//     })
+
+// });
+
+router.put('/Accept/:id', Admin, async (req, res) => {
+    const { id } = req.params;
+    const query = util.promisify(conn.query).bind(conn);
+    const requ = await query("SELECT * FROM requests WHERE id = ?", [id]);
+  
+    if (!requ)
+      res.status(404).json({ msg: "No Requests With ID = " + id });
+  
+    await query("UPDATE requests SET request_status = 1 WHERE id = ?", [id]);
+    res.status(200).json({ msg: "Request Accepted" });
+  });
+  
+
+  router.put('/Refuse/:id', Admin, async (req, res) => {
+    const { id } = req.params;
+    const query = util.promisify(conn.query).bind(conn);
+    const requ = await query("SELECT * FROM requests WHERE id = ?", [id]);
+  
+    if (!requ)
+      res.status(404).json({ msg: "No Requests With ID = " + id });
+  
+    await query("UPDATE requests SET request_status = 2 WHERE id = ?", [id]);
+    res.status(200).json({ msg: "Request Refused" });
+  });
+  
+
+
+router.get('/History/:id',Authorized,async(req,res)=>{
+    const query = util.promisify(conn.query).bind(conn);
+    // const hist = await query('SELECT * FROM history WHERE user_id = ? ',[res.locals.user.ID])
     //conn.query('SELECT * FROM requests WHERE  user_id = ?',{user_id:res.locals.user.ID},(error,result,fields)=>{
     //     res.json(res);
     // });
+    const user = await query("select * from user where id = ?",[req.params.id]);
+    if(!user[0])
+        res.status(404).json({msg:"No User With ID = "+req.params.id})
+    hist = await query("SELECT * FROM history where user_id = ?",[req.params.id]);
     res.status(200).json(hist);
 });
 module.exports = router
